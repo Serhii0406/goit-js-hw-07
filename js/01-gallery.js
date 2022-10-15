3a03f485f7889927df05ad4521f151cacf045587
Serhii0406 <106251203+Serhii0406@users.noreply.github.com>
2022-10-15 18:16:19 +0300

    part 1
    part 1, without basicLightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryDiv = document.querySelector('.gallery');
+const galleryMakrUp = createGallary (galleryItems)
 
-console.log(galleryItems);
+galleryDiv.insertAdjacentHTML('beforeend', galleryMakrUp)
+
+galleryDiv.addEventListener('click', onCardClick)
+
+function createGallary (galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `
+    })
+        .join(' ');
+}
+function onCardClick(event) {
+    event.preventDefault();
+    if (evt.target.nodeName !== "IMG") {
+        return
+    } else {
+        console.log(event.target)
+    }
+}
+
